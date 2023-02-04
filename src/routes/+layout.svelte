c199fbcafc81912a3022df4ab5ab62de5d028d58
zphyrj <46908127+zphyrj@users.noreply.github.com>
2023-02-04 14:35:46 -0800

    cleaned up moved styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <script>
-	import Nav from './Nav.svelte';
+	import Nav from './Nav.svelte'
 	import '../reset.css'
 	import '../global.css'
 	import SignIn from '$lib/SignIn.svelte'
 	import { userData } from '$stores/userData'
 </script>
 
-<Nav></Nav>
+<Nav />
 
 <main>
 	{#if $userData === null}
@@ -19,21 +19,6 @@
 </main>
 
 <style>
-	h1 {
-		color: var(--clr-gray-900);
-		white-space: pre;
-	}
-	nav {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		max-width: var(--page-width);
-		margin: 0 auto;
-		padding: var(--gap-2);
-		background-color: var(--clr-pink-700);
-		border-bottom: var(--gap-1) solid var(--clr-pink-500);
-	}
-
 	main {
 		max-width: var(--page-width);
 		margin: 0 auto;
