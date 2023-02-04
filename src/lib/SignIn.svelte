f213061f1941fd1dc714cfdd527673569f17de21
zphyrj <46908127+zphyrj@users.noreply.github.com>
2023-02-04 14:12:14 -0800

    removed unused classes in signin

--- src/lib/SignIn.svelte
+++ src/lib/SignIn.svelte
@@ -1,27 +1,27 @@
 <script lang="ts">
-	import GButton from '$lib/GButton.svelte';
-	import { GoogleAuthProvider, signInWithCredential, signOut } from 'firebase/auth';
-	import { userData, auth } from '$stores/userData';
+	import GButton from '$lib/GButton.svelte'
+	import { GoogleAuthProvider, signInWithCredential, signOut } from 'firebase/auth'
+	import { userData, auth } from '$stores/userData'
 
 	function signIn(event: CustomEvent<google.accounts.id.CredentialResponse>) {
-		loading = true;
-		const gAuthResponse = event.detail;
-		const credential = GoogleAuthProvider.credential(gAuthResponse.credential);
-		signInWithCredential(auth, credential);
+		loading = true
+		const gAuthResponse = event.detail
+		const credential = GoogleAuthProvider.credential(gAuthResponse.credential)
+		signInWithCredential(auth, credential)
 	}
 
-	let loading = $userData === undefined; // init loading value before mount
+	let loading = $userData === undefined // init loading value before mount
 
-	$: loading = $userData === undefined; // update loading value after mount
+	$: loading = $userData === undefined // update loading value after mount
 	$: greeting =
 		$userData === undefined
 			? ''
 			: $userData === null
 			? 'Sign In'
-			: `Welcome, ${$userData.displayName}`;
+			: `Welcome, ${$userData.displayName}`
 </script>
 
-<div class="card flex h-center" class:loading>
+<div class:loading>
 	<h2>{greeting}</h2>
 	{#if $userData === null}
 		<GButton on:signin={signIn} width={220} />
@@ -31,4 +31,3 @@
 		<button on:click={() => signOut(auth)}>Sign Out</button>
 	{/if}
 </div>
-
