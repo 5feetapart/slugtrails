0211c963054b29458741cc7795ddf4fa9967292e
zphyrj <46908127+zphyrj@users.noreply.github.com>
2023-02-04 13:47:24 -0800

    added login page

--- static/format.mjs
+++ static/format.mjs
@@ -1,9 +1,10 @@
 import fs from 'fs';
 
 const data = fs.readFileSync('data.json', 'utf8');
-
+const out = [];
 for (const item of JSON.parse(data)) {
 	item.tags = item.tags.split(',').map((tag) => tag.trim());
+	out.push(item);
 }
 
-fs.writeFileSync('out.json', JSON.stringify(data, null, 2), 'utf8');
+fs.writeFileSync('out.json', data, 'utf8');
