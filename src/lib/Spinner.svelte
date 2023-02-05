91d7ae4b328b450db2df13ede7ffce79473ee55d
4disease <4disease@gmail.com>
2023-02-04 16:03:25 -0800

    sign in sign out

---
+++ src/lib/Spinner.svelte
@@ -0,0 +1,25 @@
+<svg viewBox="0 0 100 100">
+    <circle cx="50" cy="50" r="40" stroke-width="10" />
+    <path d="M 10 40 C 20 20, 20 20, 40 10" stroke-width="10" />
+</svg>
+
+<style>
+    svg {
+        fill: transparent;
+        stroke: var(--clr-gray-500);
+        animation: 1s infinite linear spin;
+    }
+
+    @keyframes spin {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+
+    path {
+        stroke: var(--clr-gray-900);
+    }
+</style>
--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -3,6 +3,43 @@
 	import { auth } from '$stores/userData'
 </script>
 
+<div class="wrapper">
+<div class = "wrapper-inner">
 <h2>Settings</h2>
+<button on:click={() => signOut(auth)}>Log out</button>
+</div>
+</div>
 
-<button on:click={() => signOut(auth)}>log out</button>
+<style>
+
+	.wrapper{
+		background-color:var(--clr-gray-50);
+		background-size: cover;
+		padding:var(--gap-4);
+	}
+
+	.wrapper-inner{
+		text-align:center;
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+		margin:var(--gap-4);
+		background-color:var(--clr-pink-700);
+		padding:var(--gap-4);
+		border-radius: var(--gap-3);
+		border:var(--gap-1) solid var(--clr-pink-500);
+	}
+	h2{
+		color:var(--clr-gray-900);
+		padding:var(--gap-4);
+	}
+	button {
+		font-family: var(--ft-header);
+		color: var(--clr-gray-900);
+		background-color: var(--clr-gray-50);
+		border: var(--gap-1) solid var(--clr-pink-500);
+		padding: var(--gap-2);
+		border-radius: var(--gap-3);
+	}
+
+</style>
