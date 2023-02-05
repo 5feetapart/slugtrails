5682d49f0485a95e07c46d3d201749efb9eba0a5
nullptr3 <bencoopernotes@gmail.com>
2023-02-05 02:39:59 -0800

    zephyr adding instructions for nayan

---
+++ src/lib/Map.svelte
@@ -0,0 +1,14 @@
+<script>
+    let map: HTMLElement
+</script>
+
+<div class="map" bind:this={map}>NAYAN PUT MAP HERE</div>
+
+<script>
+    .map {
+		width: 350px;
+		height: 300px;
+		border: var(--clr-green-500) solid var(--gap-1);
+		margin: var(--gap-2);
+	}
+</script>
