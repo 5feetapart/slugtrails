65843c97a5a98ba08368376057b511a08dc85023
4disease <4disease@gmail.com>
2023-02-05 05:05:44 -0800

    ehhh

---
+++ src/lib/ActivityTracker.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import PhotoUpload from "./PhotoUpload.svelte";
+
+
+    export let inProgress: boolean = false
+</script>
+
+{#if inProgress}
+<PhotoUpload />
+Note: 
+<input type="text">
+
+<button>Complete</button>
+{:else}
+<h2>Start your workout!</h2>
+<button>Start</button>
+{/if}
---
+++ src/routes/submit/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import ActivityTracker from "$lib/ActivityTracker.svelte";
+
+</script>
+<div class="wrapper">
+<div class="wrapper-inner">
+    <ActivityTracker />
+</div>
+    
+</div>
+
+<!-- dispatch start, time.now -->
+<style>
+.wrapper{
+    margin:var(--gap-4);
+    padding:var(--gap-4);
+
+}
+
+    .wrapper-inner{
+        text-align: center;
+        justify-content:center;
+        display: flex;
+        flex-direction: column;
+        border:var(--gap-1) solid var(--clr-green-500);
+        padding:var(--gap-4);
+        border-radius: var(--gap-4);
+    }
+
+    .wrapper-inner > *{
+        padding:var(--gap-2);
+    }
+</style>
